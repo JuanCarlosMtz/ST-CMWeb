dd90b6744519cc768a01508897b1ea627d61b9c2
A00830698 <83973548+SantiM1@users.noreply.github.com>
2023-03-22 23:34:21 -0600

    Pagina de Astronomia Pt1

---
+++ src/Components/Astronomy.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+import {useEffect, useState} from 'react'
+import '../Astronomy.css'
+
+
+const apiKey = "JG92Aw10a9YLwjZ27GROTirgE6j7qnB9VeMUClvz"
+
+//const apiKey = process.env.REACT_APP_NASA_KEY
+function Astronomy() {
+    const [photoData, setPhotoData] = useState(null)
+    useEffect(() =>{
+        fetchPhoto()
+        async function fetchPhoto(){
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+            )
+            const data = await res.json()
+            setPhotoData(data)
+            console.log(data)
+        }
+    }, []);
+    return(
+        <div className="astronomy">
+            <div id="stars1"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div>
+                {!photoData ? (
+                    <h1>No Data</h1>
+                    ) : (
+                    <div className="photoData">
+                        <div className="titleData">
+                            <h1>{photoData.title}</h1>
+                            <h2>By {photoData.copyright}</h2>
+                            <h2>{photoData.date}</h2>
+                        </div>
+                        <div className="contentData">
+                             {photoData.media_type == "image"?(
+                                <img src={photoData.url} alt={photoData.title}  className="media"/>
+                             ) : (
+                                <iframe
+                                    title="astronomy-video"
+                                    src={photoData.url}
+                                    border="0"
+                                    gesture="media"
+                                    allow="encrypted-media"
+                                    allowFullScreen
+                                    className="media"
+                                />
+                             )}
+                            <p>{photoData.explanation}</p>
+                        </div>
+                </div>
+            )}
+            </div>
+
+        </div>
+    );
+}
+
+export default Astronomy
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Link} from "react-router-dom";
+
+
+function Home(){
+    return (
+        <div className="Home">
+
+            <Link to="/astronomy">Astronomy time</Link>
+
+        </div>
+
+    );
+}
+export default Home
