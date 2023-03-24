138e1166dd1757cf5a1bf39dcb137d3dbfd90ea1
Carlosmor95 <a00830435@itesm.mx>
2023-03-24 01:56:00 -0600

    funcinoes react de la pagina (Sin color de background)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Astronomy from "./Components/Astronomy.jsx"
 import Home from "./Components/Home"
 import {Link, Route, Routes} from "react-router-dom"
+import Science from './Components/Science.jsx';
 function App() {
     return(
         <>
@@ -10,11 +11,13 @@ function App() {
                 <ul>
                     <li><Link to="/" className="title">Enciclopedia</Link></li>
                     <li><Link to="/astronomy" className="link">Astronomy</Link> </li>
+                    <li><Link to="/science" className="link">Science</Link> </li>
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/astronomy" element={<Astronomy />}/>
+                <Route path="/science" element={<Science />}/>
             </Routes>
         </>
     );
